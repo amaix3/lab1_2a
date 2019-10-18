712e712ea1d8dc404baf77a0126c5303bc5f5203
Justyna Kowal <justynakowal601@gmail.com>
2019-10-18 10:16:57 +0200

    Lab3

---
+++ MN1/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,14 @@
+﻿// ConsoleApplication1.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include "pch.h"
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+    std::cout << "Hello World!\n"; 
+
+}
+
+
