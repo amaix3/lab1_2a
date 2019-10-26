6fdf2cbd79f88c90f148404baac275ece2e1576b
Justyna Kowal <justynakowal601@gmail.com>
2019-10-26 08:29:30 +0200

    Lab_4

---
+++ Lab_4/Lab_4/Kalkulator.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab_4
+{
+    public class Kalkulator
+    {
+        
+
+        public static int Dodawanie(int liczba1, int liczba2)
+        {
+
+            return liczba1 + liczba2;
+
+        }
+
+        public static int Odejmowanie(int liczba1, int liczba2)
+        {
+            return liczba1 - liczba2;
+
+
+        }
+
+        public static int Mnozenie(int liczba1, int liczba2)
+        {
+            return liczba1 * liczba2;
+
+        }
+
+        public static double Dzielenie(int liczba1, int liczba2, out bool powodzenia)
+        {
+            if (liczba2 == 0)
+            {
+                powodzenia = false;
+                return 0;
+            }
+            powodzenia = true;
+            return (double)liczba1 / liczba2;
+
+        }
+
+        public static double PodzielZWyjatkiem(int liczba1, int liczba2)
+        {
+            if (liczba2 == 0)
+            {
+                throw new DivideByZeroException();
+            }
+            return (double)liczba1 / liczba2;
+        }
+
+       public  static int Dodawanie(string x, string y)
+        {
+            int tekst1 = Convert.ToInt32(x);
+            int tekst2 = Convert.ToInt32(y);
+
+            return Dodawanie(tekst1, tekst2);
+        }
+    }
+}
---
+++ Lab_4/Lab_4/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace Lab_4
+{
+ 
+    class Program
+    {
+        static string WczytajLiczbe(string licznik)
+        {
+            Console.WriteLine("Podaj liczbę " + licznik + ": ");
+            string tekst = Console.ReadLine();
+
+            return tekst;
+
+        }
+        static void Main(string[] args)
+        {
+            string tekst1 = WczytajLiczbe("1");
+            string tekst2 = WczytajLiczbe("2");
+
+            Console.WriteLine("Wybierz operacje: ");
+            string operacja = Console.ReadLine();
+
+            int liczba1 = Convert.ToInt32(tekst1);
+            int liczba2 = Convert.ToInt32(tekst2);
+
+            switch (operacja)
+            {
+                case "+":
+                    int wynik = Kalkulator.Dodawanie(tekst1, tekst2);
+                    Console.WriteLine(wynik);
+                    break;
+
+                case "-":
+                    int wynik1 = Kalkulator.Odejmowanie(liczba1, liczba2);
+                    Console.WriteLine(wynik1);
+
+                    break;
+                case "*":
+                    int wynik2 = Kalkulator.Mnozenie(liczba1, liczba2);
+                    Console.WriteLine(wynik2);
+
+                    break;
+                case "/":
+                    /* double wynik3 = Dzielenie(liczba1, liczba2, out bool wynik5);
+
+                     if(wynik5)
+                     {
+                         Console.WriteLine(wynik3);
+                     }
+                     else
+                     {
+                         Console.WriteLine("Podzielono przez 0");
+                     } */
+
+                    try
+                    {
+                        Console.WriteLine(Kalkulator.PodzielZWyjatkiem(liczba1, liczba2));
+                    }
+                    catch (DivideByZeroException)
+                    {
+                        Console.WriteLine("Nie można dzielić przez 0.");
+                    }
+                   
+                    
+                    break;
+                default:
+                    Console.WriteLine("Wybrano zły typ operacji.");
+                    break;
+            }
+
+            Console.Read();
+        }
+    }
+}
