df2ff979b7d6b12a02032359e270d25b3d8411b7
Justyna Kowal <justynakowal601@gmail.com>
2019-11-09 09:36:49 +0100

    Lab_5

---
+++ Lab_5/Lab_5/Ksiazka.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab_5
+{
+    public class Ksiazka
+    {
+        public string Tytul;
+        public string Autor;
+
+        public int Regal;
+        public int Polka;
+        public int Miejsce;
+
+        public Ksiazka(string tytul, string autor, int regal, int polka, int miejsce)
+        {
+            Tytul = tytul;
+            Autor = autor;
+            Regal = regal;
+            Polka = polka;
+            Miejsce = miejsce;
+        }
+
+        public Ksiazka()
+        {
+
+        }
+    }
+}
