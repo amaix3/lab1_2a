766033d91cf3f7cfa3b233cdbc4158dac98613a3
Justyna Kowal <justynakowal601@gmail.com>
2020-01-31 16:07:24 +0100

    Poprawa_v2

---
+++ Poprawa_Kolokwium1_v2/Poprawa_Kolokwium1_v2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Poprawa_Kolokwium1_v2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.Read();
+        }
+    }
+}
