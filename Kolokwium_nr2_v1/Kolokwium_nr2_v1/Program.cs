d87ee8c8f5fd949b3be787b1ba7570b221863295
Justyna Kowal <justynakowal601@gmail.com>
2020-02-06 17:04:38 +0100

    Kolokwium_nr2_v1

---
+++ Kolokwium_nr2_v1/Kolokwium_nr2_v1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Kolokwium_nr2_v1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.Read();
+        }
+    }
+}
